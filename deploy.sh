08ae1814e6a340fe6eee248c78257667a740e415
Robert Kieffer <robert@broofa.com>
2018-01-20 15:49:42 -0800

    nits

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,8 @@ VERSION=$(grep -oP '(?<=version": ")[^"]+' < package.json)
 HOST=`hostname`
 
 if [ $HOST == "francis-lewis" ]; then
+  echo "Setting APP_VERSION=${VERSION}"
+
   git checkout index.html && \
     git pull && \
     sed --in-place -e "s/APP_VERSION/${VERSION}/" index.html
