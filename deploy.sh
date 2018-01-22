62152d28075c29e7008b68305fef173ac8646509
Robert Kieffer <robert@broofa.com>
2018-01-22 10:21:09 -0800

    fix: deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
-VERSION=$(grep -oP '(?<="version": ")[^"]+' < package.json)
+
+set -e
+
 HOST=`hostname`
 
-if [ $HOST == "francis-lewis" ]; then
-  echo "Setting APP_VERSION=${VERSION}"
-
-  git checkout index.html && \
-    git pull && \
-    sed --in-place -e "s/APP_VERSION/${VERSION}/" index.html
-else
+if [ $HOST != "francis-lewis" ]; then
   echo "Invalid host"
+  exit 0
 fi
+
+git checkout index.html
+git pull
+
+VERSION=$(grep -oP '(?<="version": ")[^"]+' < package.json)
+echo "Setting APP_VERSION=${VERSION}"
+sed --in-place -e "s/APP_VERSION/${VERSION}/" index.html
