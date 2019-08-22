1cd6abb25a672d07581dc2e8d2d67c3cba62fb17
刘放 <362512489@qq.com>
2019-08-22 09:44:33 +0800

    refactor: add remote mode

--- bin/npmgraph.js
+++ bin/npmgraph.js
@@ -5,19 +5,29 @@ const open = require('open');
 const argv = require('minimist')(process.argv.slice(2));
 
 const app = express();
-const port = 3003;
+const port = process.env.PORT || argv.port || 3003;
 let packageName = '';
 
 const getPackageName = (packlist = []) => {
   if (packlist.length > 0) return packlist[0];
-  console.log('Please assign packageName');
 };
 
+function usage() {
+  console.log(`Usage: npmgraph [--port=portnum] package_name
+  --port=portnum:  server port to listen on';
+  package_name: package to start dependency graph at`);
+}
+
 //handle cli params
-if (argv.local) {
-} else {
-  packageName = getPackageName(argv._);
-  if (!packageName) return;
+packageName = getPackageName(argv._);
+if (!packageName) {
+  usage();
+  process.exit(1);
+}
+//use remote mode
+if (argv.r) {
+  open(`http://npm.broofa.com?q=${packageName}`);
+  process.exit(1);
 }
 
 //view engine setup
@@ -29,5 +39,5 @@ app.use(express.static(path.join(__dirname, '../')));
 
 app.listen(port, () => {
   console.log(`npmgraph listening on port ${port}!`);
-  open('http://localhost:3003');
+  open(`http://localhost:${port}`);
 });
