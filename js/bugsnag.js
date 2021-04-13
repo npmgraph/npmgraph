93f008b078548c061da1d44a82396fdf486a84d1
Robert Kieffer <robert@broofa.com>
2021-04-13 11:22:45 -0700

    hostname-sniff for bugsnag release stage

--- js/bugsnag.js
+++ js/bugsnag.js
@@ -5,7 +5,11 @@ import { version as appVersion } from '../package.json';
 const config = {
   appVersion,
   apiKey: process.env.BUGSNAG_KEY,
-  releaseStage: process.env.NODE_ENV
+  // Note: `parcel build` always sets NODE_ENV='production'.  This isn't really correct
+  // the static files are served locally, however (e.g. `npx static-server docs`).
+  // To avoid generating bugsnag reports in dev environments, we set the stage based
+  // on hostname-sniffing.
+  releaseStage: /npmgraph/.test(window.location.hostname) ? 'production' : 'development'
 };
 
 console.log('BugSnag config:', config);
