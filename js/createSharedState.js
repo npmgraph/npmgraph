d369c06fa12ba8721bf55ceb1aaa1f2f2e5e56a9
Robert Kieffer <robert@broofa.com>
2021-04-13 07:21:41 -0700

    use parcel-supplied events module

--- js/createSharedState.js
+++ js/createSharedState.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import EventEmitter from 'eventemitter3';
+import { EventEmitter } from 'events';
 
 /**
  * A simpler, easier to use solution for sharing state across React
@@ -15,10 +15,9 @@ export default function createSharedState(defaults) {
     use(key) {
       if (!(key in defaults)) throw Error(`Shared state ${key} not declared`);
       const [val, setVal] = useState(defaults[key]);
-
       useEffect(() => {
         state.on(key, setVal);
-        return () => state.off(setVal);
+        return () => state.off(key, setVal);
       }, [val]);
 
       return [val, v => state.emit(key, v)];
