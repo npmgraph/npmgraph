028a779609c0ecd9525e64989a92dd91f85d8e9f
Robert Kieffer <robert@broofa.com>
2023-09-30 12:23:58 -0700

    fix: handle json.parse errors on upload

--- components/InfoPane/FileUploadControl.tsx
+++ components/InfoPane/FileUploadControl.tsx
@@ -8,6 +8,7 @@ import {
   PARAM_QUERY,
   UNNAMED_PACKAGE,
 } from '../../lib/constants.js';
+import { flash } from '../../lib/flash.js';
 import useLocation from '../../lib/useLocation.js';
 import './FileUploadControl.scss';
 
@@ -58,8 +59,15 @@ export default function FileUploadControl(props: HTMLProps<HTMLLabelElement>) {
       reader.readAsText(file);
     });
 
+    console.log(file);
     // Parse module and insert into cache
-    const pkg: PackageJson = JSON.parse(content);
+    let pkg: PackageJson;
+    try {
+      pkg = JSON.parse(content);
+    } catch (err) {
+      flash(`${file.name} is not a valid JSON file`);
+      return;
+    }
 
     pkg.name ??= UNNAMED_PACKAGE;
 
