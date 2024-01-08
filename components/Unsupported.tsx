4b437a538290f46f05bc81e6877e61c3c6649e1e
Robert Kieffer <robert@broofa.com>
2024-01-08 06:32:23 -0800

    fix: feature detect unsupported localStorage, fixes #202

--- components/Unsupported.tsx
+++ components/Unsupported.tsx
@@ -1,16 +1,30 @@
 import { ExternalLink } from './ExternalLink.js';
 import { GithubIcon } from './Icons.js';
 
-export function Unsupported() {
+import styles from './Unsupported.module.scss';
+
+export function Unsupported({
+  unsupported,
+  ...props
+}: { unsupported: JSX.Element[] } & React.HTMLAttributes<HTMLDivElement>) {
   return (
-    <div className="unsupported">
-      <h1>Unsupported Browser</h1>
+    <div className={styles.root} {...props}>
+      <h1>Unsupported Features</h1>
       <p>
-        NPMGraph requires a browser with modern JavaScript features. Please try
-        the latest version of Chrome, Firefox, Safari, or Edge.
+        It looks like your browser may have trouble running NPMGraph.
+        Specifically, the following features appear to be missing or disabled:
+      </p>
+      <ul>
+        {unsupported.map((feature, i) => (
+          <li key={i}>{feature}</li>
+        ))}
+      </ul>
+      <p>
+        Make sure you're using a modern version of Chrome, Firefox, Safari, or
+        Edge, and that cookies are enabled.
       </p>
 
-      <p>
+      <p className={styles.footer}>
         <ExternalLink
           href="https://github.com/npmgraph/npmgraph"
           icon={GithubIcon}
