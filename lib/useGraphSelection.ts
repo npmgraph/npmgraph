6dfc21f4092419d8b597a67ee7c3b86731930187
Robert Kieffer <robert@broofa.com>
2024-07-14 08:07:48 -0700

    fix: improved scrolling logic

--- lib/useGraphSelection.ts
+++ lib/useGraphSelection.ts
@@ -5,12 +5,12 @@ import useHashParam from './useHashParam.js';
 export default function useGraphSelection() {
   const [sel, setSel] = useHashParam(PARAM_SELECTION);
   const i = (sel ?? '').indexOf(':');
-  const type = (sel && i > 0 ? sel?.slice(0, i) : '') as QueryType;
-  const value = sel && i > 0 ? sel?.slice(i + 1) : '';
+  const selectType = (sel && i > 0 ? sel?.slice(0, i) : '') as QueryType;
+  const selectValue = sel && i > 0 ? sel?.slice(i + 1) : '';
 
   return [
-    type,
-    value,
+    selectType,
+    selectValue,
     function setGraphSelection(queryType?: QueryType, queryValue?: string) {
       if (!queryType || !queryValue) return setSel('');
       setSel(`${queryType}:${queryValue}`);
