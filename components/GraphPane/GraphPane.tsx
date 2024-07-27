1d262d80e3bc3f59ebd6130eea5f55c0e7af093f
Federico Brigante <me@fregante.com>
2024-07-27 17:12:37 +0700

    fix: hide empty sections in Graph tab (#252)

--- components/GraphPane/GraphPane.tsx
+++ components/GraphPane/GraphPane.tsx
@@ -86,84 +86,86 @@ export default function GraphPane({
         )}
       </div>
 
-      <h3>Modules</h3>
+      <div id="report-section">
+        <h3>Modules</h3>
 
-      <ReportItem data={moduleAnalysis} reporter={modulesAll} />
+        <ReportItem data={moduleAnalysis} reporter={modulesAll} />
 
-      <ReportItem data={moduleAnalysis} reporter={modulesRepeated}>
-        Module repetition is a result of incompatible version constraints, and
-        may lead to increased bundle and <code>node_modules</code> directory
-        size. Consider asking <em>upstream</em> module owners to update to the
-        latest version or loosen the version constraint.
-      </ReportItem>
+        <ReportItem data={moduleAnalysis} reporter={modulesRepeated}>
+          Module repetition is a result of incompatible version constraints, and
+          may lead to increased bundle and <code>node_modules</code> directory
+          size. Consider asking <em>upstream</em> module owners to update to the
+          latest version or loosen the version constraint.
+        </ReportItem>
 
-      <ReportItem data={moduleAnalysis} reporter={modulesDeprecated}>
-        Deprecated modules are unsupported and may have unpatched security
-        vulnerabilities. See the deprecation notes below for module-specific
-        instructions.
-      </ReportItem>
+        <ReportItem data={moduleAnalysis} reporter={modulesDeprecated}>
+          Deprecated modules are unsupported and may have unpatched security
+          vulnerabilities. See the deprecation notes below for module-specific
+          instructions.
+        </ReportItem>
 
-      <ReportItem
-        data={peerDependencyAnalysis}
-        reporter={peerDependenciesAll}
-      />
+        <ReportItem
+          data={peerDependencyAnalysis}
+          reporter={peerDependenciesAll}
+        />
 
-      <ReportItem
-        data={peerDependencyAnalysis}
-        reporter={peerDependenciesMissing}
-      />
+        <ReportItem
+          data={peerDependencyAnalysis}
+          reporter={peerDependenciesMissing}
+        />
 
-      <h3>Maintainers</h3>
+        <h3>Maintainers</h3>
 
-      <ReportItem data={maintainersAnalysis} reporter={maintainersAll} />
+        <ReportItem data={maintainersAnalysis} reporter={maintainersAll} />
 
-      <ReportItem data={maintainersAnalysis} reporter={maintainersSolo}>
-        Modules with a single maintainer are at risk of "unplanned abandonment".
-        See{' '}
-        <ExternalLink href="https://en.wikipedia.org/wiki/Bus_factor">
-          Bus factor
-        </ExternalLink>
-        .
-      </ReportItem>
+        <ReportItem data={maintainersAnalysis} reporter={maintainersSolo}>
+          Modules with a single maintainer are at risk of "unplanned
+          abandonment". See{' '}
+          <ExternalLink href="https://en.wikipedia.org/wiki/Bus_factor">
+            Bus factor
+          </ExternalLink>
+          .
+        </ReportItem>
 
-      <h3>Licenses</h3>
+        <h3>Licenses</h3>
 
-      <ReportItem data={licensesAnalysis} reporter={licensesAll} />
+        <ReportItem data={licensesAnalysis} reporter={licensesAll} />
 
-      <ReportItem
-        type="warn"
-        data={licensesAnalysis}
-        reporter={licensesMissing}
-      >
-        Modules without a declared license, or that are explicitely
-        "UNLICENSED", are not opensource and may infringe on the owner's
-        copyright. Consider contacting the owner to clarify licensing terms.
-      </ReportItem>
+        <ReportItem
+          type="warn"
+          data={licensesAnalysis}
+          reporter={licensesMissing}
+        >
+          Modules without a declared license, or that are explicitely
+          "UNLICENSED", are not opensource and may infringe on the owner's
+          copyright. Consider contacting the owner to clarify licensing terms.
+        </ReportItem>
 
-      <ReportItem
-        type="warn"
-        data={licensesAnalysis}
-        reporter={licensesKeyword('discouraged')}
-      >
-        "Discouraged" licenses typically have a more popular alternative. See{' '}
-        <ExternalLink href="https://opensource.org/licenses/">
-          OSI Licenses
-        </ExternalLink>
-        .
-      </ReportItem>
+        <ReportItem
+          type="warn"
+          data={licensesAnalysis}
+          reporter={licensesKeyword('discouraged')}
+        >
+          "Discouraged" licenses typically have a more popular alternative. See{' '}
+          <ExternalLink href="https://opensource.org/licenses/">
+            OSI Licenses
+          </ExternalLink>
+          .
+        </ReportItem>
 
-      <ReportItem
-        type="warn"
-        data={licensesAnalysis}
-        reporter={licensesKeyword('obsolete')}
-      >
-        "Obsolete" licenses have a newer version available. Consider asking the
-        module owner to update to a more recent version. See{' '}
-        <ExternalLink href="https://opensource.org/licenses/">
-          OSI Licenses
-        </ExternalLink>
-        .
-      </ReportItem>
+        <ReportItem
+          type="warn"
+          data={licensesAnalysis}
+          reporter={licensesKeyword('obsolete')}
+        >
+          "Obsolete" licenses have a newer version available. Consider asking
+          the module owner to update to a more recent version. See{' '}
+          <ExternalLink href="https://opensource.org/licenses/">
+            OSI Licenses
+          </ExternalLink>
+          .
+        </ReportItem>
+      </div>
     </Pane>
   );
 }
