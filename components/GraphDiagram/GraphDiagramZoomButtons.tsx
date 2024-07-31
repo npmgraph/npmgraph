3acae5986241ccc5396f1b638aeda8f59b3bf0cb
Federico <me@fregante.com>
2024-08-01 00:39:31 +0700

    Cleanup CSS

--- components/GraphDiagram/GraphDiagramZoomButtons.tsx
+++ components/GraphDiagram/GraphDiagramZoomButtons.tsx
@@ -12,37 +12,39 @@ import { ZoomHorizontalIcon, ZoomVerticalIcon } from '../Icons.js';
 export function GraphDiagramZoomButtons() {
   const [zoom, setZoom] = useHashParam(PARAM_ZOOM);
   return (
-    <>
+    <div id="zoom-buttons">
       <button
-        className={cn({ selected: zoom === ZOOM_FIT_WIDTH })}
+        id="zoom-fit-width"
+        className={cn({ selected: zoom === ZOOM_FIT_WIDTH }, 'bright-hover')}
         onClick={() => setZoom(ZOOM_FIT_WIDTH)}
-        title="zoom (fit width)"
-        style={{ borderRadius: '3px 0 0 3px' }}
+        title="Zoom (fit width)"
+        type="button"
       >
         <ZoomHorizontalIcon />
       </button>
       <button
-        className={cn({ selected: zoom === ZOOM_NONE })}
+        id="zoom-none"
+        className={cn({ selected: zoom === ZOOM_NONE }, 'bright-hover')}
         onClick={() => setZoom(ZOOM_NONE)}
-        title="zoom (1:1)"
+        title="Zoom (1:1)"
         style={{
           fontSize: '1em',
           padding: '0 .5em',
           width: 'fit-content',
           borderWidth: '1px 0px',
-          borderRadius: 0,
         }}
       >
         1:1
       </button>
       <button
-        className={cn({ selected: zoom === ZOOM_FIT_HEIGHT })}
+        id="zoom-fit-height"
+        className={cn({ selected: zoom === ZOOM_FIT_HEIGHT }, 'bright-hover')}
         onClick={() => setZoom(ZOOM_FIT_HEIGHT)}
-        title="zoom (fit height)"
-        style={{ borderRadius: '0 3px 3px 0' }}
+        title="Zoom (fit height)"
+        type="button"
       >
         <ZoomVerticalIcon />
       </button>
-    </>
+    </div>
   );
 }
