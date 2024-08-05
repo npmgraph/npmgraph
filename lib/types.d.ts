d5836bc0930b8830b3afc2ca1f58ed7bb8b76c28
Federico <me@fregante.com>
2024-08-05 15:06:16 +0700

    Lint

--- lib/types.d.ts
+++ lib/types.d.ts
@@ -5,5 +5,5 @@ declare module 'bundle-text:*' {
   export default value;
 }
 
-declare module "*.module.css";
-declare module "*.module.scss";
+declare module '*.module.css';
+declare module '*.module.scss';
