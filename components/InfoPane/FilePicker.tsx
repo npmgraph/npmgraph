194e6e799b0aab8b04adf9798a356683180f87ee
Federico <me@fregante.com>
2024-08-09 20:12:02 +0700

    feat: drop files anywhere, any tab

---
+++ components/InfoPane/FilePicker.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { readFile } from '../../lib/read_file.js';
+
+// Handle file selection via input
+function onSelect(ev: React.ChangeEvent<HTMLInputElement>) {
+  const file = ev.target.files?.item(0);
+  if (file) {
+    readFile(file);
+  }
+
+  // Reset field
+  ev.target.value = '';
+}
+
+export default function FilePicker({ label }: { label: string }) {
+  return (
+    <label className="link" style={{ display: 'inline' }}>
+      {label}
+      <input type="file" hidden onChange={onSelect} accept=".json" />
+    </label>
+  );
+}
