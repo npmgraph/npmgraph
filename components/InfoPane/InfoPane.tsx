7fe77dca3c17ff0a195759c2a7d081c24027661e
Federico Brigante <me@fregante.com>
2024-08-13 00:58:48 +0700

    Drop "npmgraph" from footer
    Co-authored-by: Robert Kieffer <robert@broofa.com>

--- components/InfoPane/InfoPane.tsx
+++ components/InfoPane/InfoPane.tsx
@@ -41,8 +41,6 @@ export default function InfoPane(props: HTMLProps<HTMLDivElement>) {
       <hr />
       <footer>
         <p>
-          npmgraph
-          {' | '}
           <a
             href="https://github.com/npmgraph/npmgraph"
             target="_blank"
