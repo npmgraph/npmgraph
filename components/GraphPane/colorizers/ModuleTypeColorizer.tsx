bf8c43a58fc9874c940ab39a0c521ff733fbf643
Robert Kieffer <robert@broofa.com>
2025-06-17 08:19:46 -0700

    logic fixes

--- components/GraphPane/colorizers/ModuleTypeColorizer.tsx
+++ components/GraphPane/colorizers/ModuleTypeColorizer.tsx
@@ -17,16 +17,20 @@ export default {
         <LegendColor color={COLORIZE_MODULE_CJS}>CJS Only</LegendColor>
         <LegendColor color={COLORIZE_MODULE_DUAL}>CJS and ESM</LegendColor>
         <LegendColor color={COLORIZE_MODULE_ESM}>ESM Only</LegendColor>
-        <LegendColor color={COLORIZE_MODULE_TYPES}>TS Types</LegendColor>
+        <LegendColor color={COLORIZE_MODULE_TYPES}>
+          TypeScript types
+        </LegendColor>
       </>
     );
   },
 
   async colorForModule(module: Module) {
     const pkgType = detectPackageType(module.package as PackageJSON);
+
     if (pkgType.esm && pkgType.cjs) {
       return COLORIZE_MODULE_DUAL;
     }
+
     if (pkgType.esm) {
       return COLORIZE_MODULE_ESM;
     }
@@ -41,12 +45,6 @@ export default {
   },
 };
 
-type PackageModuleType = {
-  esm: boolean;
-  cjs: boolean;
-  types: boolean;
-};
-
 // Package-type detection logic
 //
 // Logic to look at a PackageJSON object and determine if it is an ESM or CJS
@@ -54,6 +52,21 @@ type PackageModuleType = {
 //
 // Ref:
 // https://github.com/npmgraph/npmgraph/pull/326#issuecomment-2972640439
+
+type PackageModuleType = {
+  esm: boolean;
+  cjs: boolean;
+  types: boolean;
+};
+
+function isESMFile(file?: string) {
+  return file?.endsWith?.('.mjs') || file?.endsWith?.('.mts');
+}
+
+function isCJSFile(file?: string) {
+  return file?.endsWith?.('.cjs') || file?.endsWith?.('.cts');
+}
+
 function detectPackageType(pkg: PackageJSON) {
   // If the package name starts with @types/, then it's TS-types only
   if (pkg.name.startsWith?.('@types/')) {
@@ -72,8 +85,8 @@ function detectPackageType(pkg: PackageJSON) {
   };
 
   // Inspect package#main
-  if (pkg.main?.endsWith?.('.mjs')) pkgType.esm = true;
-  if (pkg.main?.endsWith?.('.cjs')) pkgType.cjs = true;
+  if (isESMFile(pkg.main)) pkgType.esm = true;
+  if (isCJSFile(pkg.main)) pkgType.cjs = true;
 
   // Inspect package#exports (recursively)
   return _detectExports(pkg.exports, pkgType);
@@ -81,25 +94,37 @@ function detectPackageType(pkg: PackageJSON) {
 
 // Loosely inspect package.json#exports for module type (recursive)
 function _detectExports(exports: unknown, pkgType: PackageModuleType) {
+  if (!exports) return pkgType;
+
+  // The presence of .mjs, .mts, .cjs, or .cts files is a strong indicator of
+  // the module type
+  if (typeof exports === 'string') {
+    if (isESMFile(exports)) pkgType.esm = true;
+    if (isCJSFile(exports)) pkgType.cjs = true;
+    return pkgType;
+  }
+
+  // Drill into array values
   if (Array.isArray(exports)) exports.some(v => _detectExports(v, pkgType));
 
-  if (exports && typeof exports === 'object') {
-    for (const [k, v] of Object.entries(exports)) {
-      if (k === 'import') {
-        pkgType.esm = true;
-      } else if (k === 'require') {
-        pkgType.cjs = true;
-      } else if (k === 'default') {
-        pkgType.esm = true;
-        pkgType.cjs = true;
-      }
+  if (typeof exports === 'object') {
+    const defaultVal = 'default' in exports && exports.default;
+    const importVal = 'import' in exports && exports.import;
+    const requireVal = 'require' in exports && exports.require;
 
+    // Infer dual support if there's an explicit import or require in
+    // combination with a default export
+    if (importVal || (defaultVal && requireVal)) pkgType.esm = true;
+    if (requireVal || (defaultVal && importVal)) pkgType.cjs = true;
+
+    // Walk through the exports object
+    for (const [k, v] of Object.entries(exports)) {
       if (typeof v === 'string') {
-        if (v?.endsWith('.mjs')) pkgType.esm = true;
-        if (v?.endsWith('.cjs')) pkgType.cjs = true;
-      } else {
         _detectExports(v, pkgType);
       }
+
+      // Recurse into "." exports
+      if (k === '.') _detectExports(v, pkgType);
     }
   }
 
