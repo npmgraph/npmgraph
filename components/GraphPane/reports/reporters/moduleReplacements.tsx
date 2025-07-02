npmgraph/npmgraph

commit 350bffcc6fc5fda84ef056b121af23ba7854134f

feat: support custom registry endpoints (#332)
Adds a field to the Start tab for [optionally] specifying a custom
registry. This will allow organizations that host their own NPM
registries (e.g. with [Verdaccio](https://verdaccio.org/)) to run
registry requests through those servers rather than registry.npmjs.org.

And for a bit of extra sugar, adds a registry health check indicator
(based on whether or not a request to "/_" returns a response).

The `registry` state is stored as a search (not hash) param in the URL,
since this field affects the actual state available to the graph, not
just how it's viewed.

> [!NOTE]
> The configured registry is responsible for resolving all module
requests. I.e. if the registry is set to a private server, that server
should proxy requests for public NPM modules to `registry.npmjs.org` (or
other public mirror).

Closes #19

lib/ModuleCache.ts
@@ -20,15 +20,15 @@ import {
   resolveModule,
 } from './module_util.js';
 import { hashGet } from './url_util.js';
-
-export const REGISTRY_BASE_URL = 'https://registry.npmjs.org';
+import { getRegistry } from './useRegistry.js';
 
 const moduleCache = new Map<string, ModuleCacheEntry>();
 
 export type QueryType = 'exact' | 'name' | 'license' | 'maintainer';
 
 type ModuleCacheEntry = PromiseWithResolversType<Module> & {
-  module?: Module; // Set once module is loaded
+  module: Module; // Set once module is loaded
+  registry?: string; // NPM_REGISTRY url
 };
 
 function selectVersion(
@@ -79,7 +79,7 @@ async function fetchModuleFromURL(urlString: string) {
 
   // TODO: We should probably be fetching github content via their REST API, but
   // that makes this code much more github-specific.  So, for now, we just do
-  // some URL-messaging to pull from the "raw" URL
+  // some URL-massaging to pull from the "raw" URL
   if (/\.?github.com$/.test(url.host)) {
     url.host = 'raw.githubusercontent.com';
     url.pathname = url.pathname.replace('/blob', '');
@@ -109,7 +109,7 @@ export async function getModule(moduleKey: string): Promise<Module> {
   moduleKey = getModuleKey(name, version);
   // Check cache once we're done massaging the version string
   const cachedEntry = moduleCache.get(moduleKey);
-  if (cachedEntry) {
+  if (cachedEntry && cachedEntry.registry === getRegistry()) {
     return cachedEntry.promise;
   }
 
@@ -125,6 +125,7 @@ export async function getModule(moduleKey: string): Promise<Module> {
   if (isHttpModule(moduleKey)) {
     promise = fetchModuleFromURL(moduleKey);
   } else {
+    cacheEntry.registry = getRegistry();
     promise = fetchModuleFromNPM(name, version);
   }
   promise
@@ -149,19 +150,21 @@ export async function getModule(moduleKey: string): Promise<Module> {
 }
 
 export function getCachedModule(key: string) {
-  return moduleCache.get(key)?.module;
+  const entry = moduleCache.get(key);
+  return entry && entry.registry === getRegistry() ? entry.module : undefined;
 }
 
-export function cacheModule(module: Module) {
+export function cacheModule(module: Module, registry?: string) {
   const moduleKey = module.key;
   const entry = moduleCache.get(moduleKey);
 
-  if (entry) {
+  if (entry && entry?.registry === registry) {
     entry.resolve(module);
   } else {
     moduleCache.set(moduleKey, {
       promise: Promise.resolve(module),
       module,
+      registry,
       resolve() {},
       reject() {},
     });

lib/PackumentCache.ts
@@ -1,9 +1,9 @@
 import type { Packument } from '@npm/types';
-import { REGISTRY_BASE_URL } from './ModuleCache.js';
 import PromiseWithResolvers, {
   type PromiseWithResolversType,
 } from './PromiseWithResolvers.js';
 import fetchJSON from './fetchJSON.js';
+import { getRegistry } from './useRegistry.js';
 
 const packumentCache = new Map<string, PackumentCacheEntry>();
 
@@ -16,12 +16,15 @@ type PackumentCacheEntry = PromiseWithResolversType<Packument | undefined> & {
 export async function getNPMPackument(
   moduleName: string,
 ): PackumentCacheEntry['promise'] {
-  let cacheEntry = packumentCache.get(moduleName);
+  const registry = getRegistry();
+  const url = `${registry}/${moduleName}`;
+
+  let cacheEntry = packumentCache.get(url);
   if (!cacheEntry) {
     cacheEntry = PromiseWithResolvers() as PackumentCacheEntry;
     packumentCache.set(moduleName, cacheEntry);
 
-    await fetchJSON<Packument>(`${REGISTRY_BASE_URL}/${moduleName}`, {
+    await fetchJSON<Packument>(`${registry}/${moduleName}`, {
       // Per
       // https://github.com/npm/registry/blob/master/docs/responses/package-metadata.md
       // we should arguably be using the 'Accept:
@@ -53,7 +56,9 @@ export function getCachedPackument(moduleName: string): Packument | undefined {
 }
 
 export function cachePackument(moduleName: string, packument: Packument): void {
-  let cacheEntry = packumentCache.get(moduleName);
+  const url = `${getRegistry()}/${moduleName}`;
+
+  let cacheEntry = packumentCache.get(url);
   if (!cacheEntry) {
     cacheEntry = PromiseWithResolvers() as PackumentCacheEntry;
     packumentCache.set(moduleName, cacheEntry);

components/GraphDiagram/GraphDiagram.tsx
@@ -27,6 +27,7 @@ import useCollapse from '../../lib/useCollapse.js';
 import useGraphSelection from '../../lib/useGraphSelection.js';
 import useHashParam from '../../lib/useHashParam.js';
 import { useQuery } from '../../lib/useQuery.js';
+import useRegistry from '../../lib/useRegistry.js';
 import {
   getColorizer,
   isSimpleColorizer,
@@ -56,6 +57,7 @@ export default function GraphDiagram({ activity }: { activity: LoadActivity }) {
   const [, setZenMode] = useHashParam(PARAM_HIDE);
   const [selectType, selectValue, setGraphSelection] = useGraphSelection();
   const [graph, setGraph] = useGlobalState('graph');
+  const [registry] = useRegistry();
 
   const [collapse, setCollapse] = useCollapse();
   const [colorize] = useHashParam(PARAM_COLORIZE);
@@ -162,7 +164,6 @@ export default function GraphDiagram({ activity }: { activity: LoadActivity }) {
   // Effect: Fetch modules
   useEffect(() => {
     const { signal, abort } = createAbortable();
-
     getGraphForQuery(query, dependencyTypes, moduleFilter).then(newGraph => {
       if (signal.aborted) return; // Check after async
 
@@ -173,10 +174,16 @@ export default function GraphDiagram({ activity }: { activity: LoadActivity }) {
 
       setRootScrolling(true);
       setGraph(newGraph);
+      console.log('GraphDiagram: getGraphForQuery', registry, newGraph);
     });
 
     return abort;
-  }, [[...query].sort().join(), [...dependencyTypes].join(), collapse]);
+  }, [
+    [...query].sort().join(),
+    [...dependencyTypes].join(),
+    collapse,
+    registry,
+  ]);
 
   // Effect: Insert SVG markup into DOM
   useEffect(() => {

components/GraphDiagram/graph_util.ts
@@ -2,6 +2,7 @@ import simplur from 'simplur';
 import type Module from '../../lib/Module.js';
 import { getModule } from '../../lib/ModuleCache.js';
 import { getModuleKey } from '../../lib/module_util.js';
+import { PARAM_QUERY } from '../../lib/constants.js';
 
 const FONT = 'Roboto Condensed, sans-serif';
 
@@ -219,7 +220,7 @@ export function composeDOT({
     }
 
     const link = new URL(location.origin);
-    link.searchParams.append('q', module.key);
+    link.searchParams.append(PARAM_QUERY, module.key);
     const vs = { root: level === 0, fontsize, href: link.href };
 
     nodes.push(`"${dotEscape(module.key)}" ${vizStyle(vs)}`);

components/Icons.tsx
@@ -1,4 +1,4 @@
-import React, { type HTMLProps, type SVGProps } from 'react';
+import type { HTMLProps, SVGProps } from 'react';
 import { cn } from '../lib/dom.js';
 
 import './Icons.scss';
@@ -68,6 +68,15 @@ export function GithubIcon(props: IconProps) {
     <Icon
       {...props}
       fillPath="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"
+    />
+  );
+}
+
+export function PencilIcon(props: IconProps) {
+  return (
+    <Icon
+      {...props}
+      fillPath="M11.013 1.427a1.75 1.75 0 0 1 2.474 0l1.086 1.086a1.75 1.75 0 0 1 0 2.474l-8.61 8.61c-.21.21-.47.364-.756.445l-3.251.93a.75.75 0 0 1-.927-.928l.929-3.25c.081-.286.235-.547.445-.758l8.61-8.61Zm.176 4.823L9.75 4.81l-6.286 6.287a.253.253 0 0 0-.064.108l-.558 1.953 1.953-.558a.253.253 0 0 0 .108-.064Zm1.238-3.763a.25.25 0 0 0-.354 0L10.811 3.75l1.439 1.44 1.263-1.263a.25.25 0 0 0 0-.354Z"
     />
   );
 }

components/InfoPane/InfoPane.tsx
@@ -1,8 +1,9 @@
-import React, { type HTMLProps } from 'react';
+import type { HTMLProps } from 'react';
 import { Pane } from '../Pane.js';
 import { QueryLink } from '../QueryLink.js';
 import FilePicker from './FilePicker.js';
 import QueryInput from './QueryInput.js';
+import RegistryInput from './RegistryInput.js';
 
 export default function InfoPane(props: HTMLProps<HTMLDivElement>) {
   return (
@@ -37,6 +38,8 @@ export default function InfoPane(props: HTMLProps<HTMLDivElement>) {
           <FilePicker label="Choose file" /> from your computer
         </li>
       </ul>
+
+      <RegistryInput />
 
       <hr />
       <footer>

components/InfoPane/QueryInput.tsx
@@ -1,5 +1,5 @@
 import React, { type HTMLProps, useRef, useState } from 'react';
-import { UNNAMED_PACKAGE } from '../../lib/constants.js';
+import { PARAM_QUERY, UNNAMED_PACKAGE } from '../../lib/constants.js';
 import { isDefined } from '../../lib/guards.js';
 import { searchSet } from '../../lib/url_util.js';
 import { patchLocation } from '../../lib/useLocation.js';
@@ -33,7 +33,7 @@ export default function QueryInput(props: HTMLProps<HTMLInputElement>) {
       .filter(isDefined);
 
     moduleKeys = [...new Set(moduleKeys)]; // De-dupe
-    return searchSet('q', moduleKeys.join(', '));
+    return searchSet(PARAM_QUERY, moduleKeys.join(', '));
   }
 
   function handleSubmit(e?: React.FormEvent<HTMLFormElement>) {

components/InfoPane/RegistryInput.module.scss
@@ -1,0 +1,28 @@
+.root {
+  align-items: center;
+  margin-top: 1.5rem;
+
+  > span {
+    display: inline-block;
+    flex-grow: 0;
+    margin-right: 0.5rem;
+  }
+
+  > input {
+    flex-grow: 1;
+    box-sizing: border-box;
+    border: solid 1px var(--text-dim);
+    field-sizing: content;
+    outline: none;
+    border-radius: 1em;
+    padding: 0.3em 0.7em;
+    font: inherit;
+  }
+}
+
+.status {
+  display: inline-block;
+  flex-grow: 0;
+  margin-left: 0.5rem;
+  color: var(--text-dim);
+}

commit 9fe915abb2fa59fbc2113e1192e60cd6ad3b04ed

fix: packument cache bug (#335)
Fix a bug introduced in #332 that broke package.json uploads.

components/GraphPane/reports/reporters/moduleReplacements.tsx
@@ -27,10 +27,6 @@ for (const { moduleReplacements } of [native, microUtilities, preferred]) {
   }
 }
 
-for (const r of REPLACEMENTS.values()) {
-  console.log(r.category, r.type);
-}
-
 export function moduleReplacementsNative({
   moduleInfos,
   // entryModules,

lib/ModuleCache.ts
@@ -227,6 +227,7 @@ export function sanitizePackageKeys(pkg: PackageJSON) {
 export function cacheLocalPackage(pkg: PackumentVersion) {
   let packument = getCachedPackument(pkg.name);
   if (!packument) {
+    const now = new Date().toISOString();
     // Create a stub packument
     packument = {
       name: pkg.name,
@@ -234,8 +235,9 @@ export function cacheLocalPackage(pkg: PackumentVersion) {
       'dist-tags': {},
       maintainers: [],
       time: {
-        modified: new Date().toISOString(),
-        created: new Date().toISOString(),
+        modified: now,
+        created: now,
+        [pkg.version]: now,
       },
       license: pkg.license ?? 'UNLICENSED',
     } as unknown as Packument;

lib/PackumentCache.ts
@@ -7,21 +7,25 @@ import { getRegistry } from './useRegistry.js';
 
 const packumentCache = new Map<string, PackumentCacheEntry>();
 
-export type QueryType = 'exact' | 'name' | 'license' | 'maintainer';
-
 type PackumentCacheEntry = PromiseWithResolversType<Packument | undefined> & {
   packument?: Packument; // Set once packument is loaded
+  registry?: string; // NPM_REGISTRY url
 };
 
 export async function getNPMPackument(
   moduleName: string,
 ): PackumentCacheEntry['promise'] {
   const registry = getRegistry();
-  const url = `${registry}/${moduleName}`;
 
-  let cacheEntry = packumentCache.get(url);
+  let cacheEntry = packumentCache.get(moduleName);
+  // Invalidate cache if registry has changed
+  if (cacheEntry?.registry && cacheEntry.registry !== registry) {
+    cacheEntry = undefined;
+  }
+
   if (!cacheEntry) {
     cacheEntry = PromiseWithResolvers() as PackumentCacheEntry;
+    cacheEntry.registry = registry;
     packumentCache.set(moduleName, cacheEntry);
 
     await fetchJSON<Packument>(`${registry}/${moduleName}`, {
@@ -56,9 +60,7 @@ export function getCachedPackument(moduleName: string): Packument | undefined {
 }
 
 export function cachePackument(moduleName: string, packument: Packument): void {
-  const url = `${getRegistry()}/${moduleName}`;
-
-  let cacheEntry = packumentCache.get(url);
+  let cacheEntry = packumentCache.get(moduleName);
   if (!cacheEntry) {
     cacheEntry = PromiseWithResolvers() as PackumentCacheEntry;
     packumentCache.set(moduleName, cacheEntry);