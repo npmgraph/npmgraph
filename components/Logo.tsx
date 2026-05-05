a102b9bef73a10ab6e65d851b126a33811271d63
fregante <me@fregante.com>
2026-05-05 15:42:41 +0700

    fix: intro logo size (#383)

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -2,7 +2,7 @@ import RawLogo from 'jsx:../images/logo.svg';
 import * as styles from './Logo.module.scss';
 
 function Logo() {
-  return <RawLogo className={styles.root} />;
+  return <RawLogo viewBox="0 0 545.7 100" className={styles.root} />;
 }
 
 export default Logo;
